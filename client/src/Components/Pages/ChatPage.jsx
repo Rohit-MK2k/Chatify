634bcdcf9b33363e87ed2c964f3ff8c884f5fc66
Rohit-MK2K <rohitmalakar007@gmail.com>
2023-07-03 11:27:47 +0530

    inital Schema and ChatPage UI

--- client/src/Components/Pages/ChatPage.jsx
+++ client/src/Components/Pages/ChatPage.jsx
@@ -1,9 +1,22 @@
 import React from 'react'
+import { IoSend} from "react-icons/io5";
 
 function ChatPage() {
+  const messageForm = 'flex flex-row border-2 rounded-3xl w-full p-3'
+  const messageBox = 'pr-3 bg-transparent w-fit grow focus:outline-none'
   return (
       <>
-          
+        <div className="flex flex-row m-auto px-3 py-4 justify-center items-cente w-screen h-screen ">
+          <div className="flex flex-col w-3/12 border-2 rounded-2xl p-2">
+              <div>Chatify </div>
+          </div>
+          <div className="flex flex-col justify-end w-9/12 p-2 relative">
+            <form action="" className={messageForm}>
+              <input type="text" id="chat" className={messageBox} placeholder='Message' />
+              <button type="submit" className='p-2 '><IoSend /></button>
+            </form>
+          </div>
+        </div>
       </>
   )
 }
